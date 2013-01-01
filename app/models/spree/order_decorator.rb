bc85fb1fa9098ed10ab9ba4add3966b518a94524
Reed Law <reed@smashingboxes.com>
2013-01-01 16:07:53 -0500

    remove spree

--- app/models/spree/order_decorator.rb
+++
@@ -1,8 +0,0 @@
-Spree::Order.class_eval do
-  checkout_flow do
-    go_to_state :payment, :if => lambda { |order| order.payment_required? }
-    go_to_state :confirm, :if => lambda { |order| order.confirmation_required? }
-    go_to_state :complete
-    remove_transition :from => :delivery, :to => :confirm
-  end
-end
