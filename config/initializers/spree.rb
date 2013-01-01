33442d852df5bfc7f2a2635130c0bd41d77e5cd3
Reed Law <reed@smashingboxes.com>
2013-01-01 10:44:21 -0500

    remove addresses

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -10,6 +10,7 @@ Spree.config do |config|
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
   config.set(logo: "logo.png")
+  config.set(allow_guest_checkout: false)
 end
 
 SslRequirement.module_eval do
